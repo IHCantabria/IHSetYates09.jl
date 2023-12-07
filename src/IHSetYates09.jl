fa97b767fc66922ba7c32e1465a9660686035510
defreitasL <lucas12.fp@hotmail.com>
2023-12-07 12:02:36 +0100

    Updating Pkg

--- src/IHSetYates09.jl
+++ src/IHSetYates09.jl
@@ -1,7 +1,8 @@
 module IHSetYates09
 
-export Yates09, Yates09Vitousek21Mod
-include("Yates09.jl")
+using IHSetUtils
+export run_Yates09
 
+include("Yates09.jl")
 
 end
--- src/Yates09.jl
+++ src/Yates09.jl
@@ -1,3 +1,81 @@
+function run_Yates09()
+
+    println("Loading libraries...")
+    wrkDir = pwd()
+    mods = wrkDir*"Modules\\"
+    dats = wrkDir*"Data\\"
+
+    # mods = wrkDir*"\\Modules\\"
+    # dats = wrkDir*"\\Data\\"
+
+    println("Loading datasets...")
+
+    wavF = NCDataset(dats*"wav.nc")
+    ensF = NCDataset(dats*"ens.nc")
+    parF = NCDataset(dats*"par.nc")
+
+    configF = NCDataset(dats*"config.nc")
+
+    println("Unpacking datasets...")
+
+    dt = configF["dt"][:][1]
+    
+    yi = configF["yi"][:][1]
+
+    a = parF["a"][:][1]
+    b = parF["b"][:][1]
+    cacr = parF["cacr"][:][1]
+    cero = parF["cero"][:][1]
+    
+
+    
+    brk, angBati, depth, Hberm, D50 = configF["brk"][:][1], configF["angBati"][:][1], configF["depth"][:][1], configF["Hberm"][:][1], configF["D50"][:][1]
+
+    if brk == 1
+        
+        Hs, Tp, θ_w = collect(skipmissing(wavF["Hs"][:])), collect(skipmissing(wavF["Tp"][:])), collect(skipmissing(wavF["Dir"][:]))
+
+        auxAng, auxDepth = similar(Hs), similar(Hs)
+        auxAng .= angBati
+        auxDepth .= depth
+
+        println("Breaking waves by linear theory...")
+        Hb, θ_b, depthb = BreakingPropagation(Hs, Tp, θ_w, auxAng, auxDepth, "spectral")
+    else
+        Hb, Tp, Hs, depthb = wavF["Hb"][:], wavF["Tp"][:], wavF["Hs"][:], wavF["hb"][:]
+    end
+    
+    close(wavF)
+    close(ensF)
+    close(configF)
+    close(parF)
+
+    println("Datasets closed...")
+
+    Hs = convert(Array{Float64},Hs)
+    Tp = convert(Array{Float64},Tp)
+
+    a = convert(Array{Float64},a)
+    b = convert(Array{Float64},b)
+    cacr = convert(Array{Float64},kacr)
+    cero = convert(Array{Float64},kero)
+
+    Yi = convert(Array{Float64},yi)
+
+    ########## START HERE #############
+
+    E = Hb .^ 2
+
+    println("Starting Jaramillo et al. (2021) - Shoreline Rotation Model...")
+
+
+    Y_t = (E, dt, a, b, cacr, cero, Yi)
+
+    println("\n\n****************Finished****************\n\n")
+
+    return Y_t
+    
+end
 
 function Yates09(E, dt, a, b, cacr, cero, Yini)
     
