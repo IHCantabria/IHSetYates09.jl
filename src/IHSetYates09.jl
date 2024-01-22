ba7981b4e6da9e1c8eb74c5d61e1fe0f45fbf675
defreitasL <lucas12.fp@hotmail.com>
2024-01-22 09:48:04 +0100

    Updt Pkg

--- src/IHSetYates09.jl
+++ src/IHSetYates09.jl
@@ -1,8 +1,10 @@
 module IHSetYates09
 
 using IHSetUtils
-export run_Yates09
-
+using NetCDF
+using BlackBoxOptim
+using Dates
+export run_Yates09, cal_Yates09
 include("Yates09.jl")
 
 end
--- src/Yates09.jl
+++ src/Yates09.jl
@@ -1,18 +1,36 @@
+"""
+#######################################
+# Yates et al. (2004) model           #
+# E: wave energy                      #
+# dt: time step                       #
+# a: model's parameter                #
+# b: model's parameter                #
+# kero: erosion coefficient           #
+# kacr: accretion coefficient         #
+# Yi: initial position                #
+#######################################
+#######################################
+# Vitousek et al. (2021) model        #
+# Hs: wave significant height         #
+# dt: time step                       #
+# H_s: model's parameter              #
+# DY: model's parameter               #
+# DT: model's parameter               #
+# Yi: initial position                #
+#######################################
+"""
+
 function run_Yates09()
 
     println("Loading libraries...")
     wrkDir = pwd()
-    mods = wrkDir*"Modules\\"
-    dats = wrkDir*"Data\\"
-
-    # mods = wrkDir*"\\Modules\\"
-    # dats = wrkDir*"\\Data\\"
 
     println("Loading datasets...")
 
-    wavF = NCDataset(dats*"wav.nc")
-    ensF = NCDataset(dats*"ens.nc")
-    parF = NCDataset(dats*"par.nc")
+    wavF = dats*"wav.nc"
+    parF = dats*"par.nc"
+    slF = dats*"sl.nc"
+    configF = dats*"config.nc"
 
     configF = NCDataset(dats*"config.nc")
 
@@ -79,7 +97,7 @@ end
 
 function Yates09(E, dt, a, b, cacr, cero, Yini)
     
-    @views Seq = (E .- b) ./ a
+    Seq = (E .- b) ./ a
     
     Y = similar(E)
     
@@ -98,7 +116,7 @@ end
 
 function Yates09Vitousek21Mod(hs, dt, H_s, DY, DT, Yini)
     
-    Y = similar(hs)
+    Y = zeros(length(hs))
     
     Y[1] = Yini
 
@@ -111,5 +129,439 @@ function Yates09Vitousek21Mod(hs, dt, H_s, DY, DT, Yini)
     end
 
     return Y, Yeq
+
+end
+
+function cal_Yates09()
+
+    println("Loading libraries...")
+    wrkDir = pwd()
+    dats = wrkDir*"/data/"
+
+    # mods = wrkDir*"\\Modules\\"
+    # dats = wrkDir*"\\Data\\"
+
+    println("Reading datasets...")
+
+    wavF = dats*"wav.nc"
+    ensF = dats*"ens.nc"
+    parF = dats*"par.nc"
+
+    configF = dats*"config.nc"
+
+    dt = ncread(configF, "dt")[1]
+    
+    Yi = ncread(configF, "Yi")[1]
+
+    brk, angBati, depth, Hberm, D50 = ncread(configF, "brk")[1], ncread(configF, "angBati")[1], ncread(configF, "depth")[1], ncread(configF, "Hberm")[1], ncread(configF, "D50")[1]
+
+    flagP = ncread(configF, "flagP")[1]
+
+    MetObj = ncread(configF, "MetObj")[1]
+
+    if brk == 1
+        
+        Hs, Tp, θ_w = ncread(wavF, "Hs"), ncread(wavF, "Tp"), ncread(wavF, "Dir")
+
+        auxAng, auxDepth = similar(Hs), similar(Hs)
+        auxAng .= angBati
+        auxDepth .= depth
+
+        println("Breaking waves by linear theory...")
+        Hb, θ_b, depthb = BreakingPropagation(Hs, Tp, θ_w, auxAng, auxDepth, "spectral")
+    else
+        Hb, Tp, Hs, depthb = ncread(wavF, "Hb"), ncread(wavF, "Tp"), ncread(wavF, "Hs"), ncread(wavF, "hb")
+    end
+
+    YY, MM, DD, HH = ncread(wavF, "Y"), ncread(wavF, "M"), ncread(wavF, "D"), ncread(wavF, "h")
+
+    YYo, MMo, DDo, HHo = ncread(ensF, "Y"), ncread(ensF, "M"), ncread(ensF, "D"), ncread(ensF, "h")
+    
+    Y_obs = ncread(ensF, "Obs")
+
+    t_obs = DateTime.(YYo, MMo, DDo, HHo)
+    t_wav = DateTime.(YY, MM, DD, HH)
+
+    ii =  t_obs .<= t_wav[end] .&& t_obs .>= t_wav[1]
+
+    t_obs, Y_obs = t_obs[ii], Y_obs[ii]
+
+    ii =  t_wav .<= t_obs[end] .&& t_wav .>= t_obs[1]
+
+    t_wav, hb, tp, sl, hs, depthb = t_wav[ii], Hb[ii], Tp[ii], sl[ii], Hs[ii], depthb[ii]
+
+    idx_obs = zeros(length(t_obs))
+
+    for i in eachindex(t_obs)
+        idx_obs[i] = findall((x)-> x == t_obs[i], t_wav)[1]
+    end
+
+    idx_obs = convert(Array{Int64},idx_obs)
+
+    ########## START HERE #############
+
+    E = Hb .^ 2
+
+    println("Starting Yates et al. (2009) - Shoreline Evolution Model...")
+
+    Ymdr = Dict()
+    aRP = Dict()
+    aRMSE = Dict()
+    aMSS = Dict()
+    popr = Dict()
+    objr = Dict()
+
+    if calPar == 4
+        function Calibra_MDr(Χ)
+
+            Ymd = Yates09(E, dt, -exp(Χ[1]), exp(Χ[2]), -exp(Χ[3]), -exp(Χ[4]), Y_obs[1])
+            YYsl = Ymd[idx_obs]
+            if MetObj == "Pearson"
+                return 1 -  abs(sum((YYsl.-mean(YYsl)).*(Y_obs .- mean(Y_obs)))/(std(YYsl)*std(Y_obs)*length(YYsl)))
+            elseif MetObj == "RMSE"
+                return abs(sqrt(mean((YYsl .- Y_obs).^2))/5)
+            elseif MetObj == "MSS"
+                return sum((YYsl .- Y_obs).^2)/length(YYsl)/(var(YYsl)+var(Y_obs)+(mean(YYsl)-mean(Y_obs))^2)
+            elseif MetObj == "BSS"
+                return (mean((YYsl .- Y_obs).^2) - mean((YYref .- Y_obs).^2))/mean((YYref .- Y_obs).^2)
+            elseif MetObj == "Double"
+                return (sum((YYsl .- Y_obs).^2)/length(YYsl)/(var(YYsl)+var(Y_obs)+(mean(YYsl)-mean(Y_obs))^2), abs(sqrt(mean((YYsl .- Y_obs).^2))/5))
+            elseif MetObj == "Triple"
+                return (sum((YYsl .- Y_obs).^2)/length(YYsl)/(var(YYsl)+var(Y_obs)+(mean(YYsl)-mean(Y_obs))^2), abs(sqrt(mean((YYsl .- Y_obs).^2))/5), 1 -  abs(sum((YYsl.-mean(YYsl)).*(Y_obs .- mean(Y_obs)))/(std(YYsl)*std(Y_obs)*length(YYsl))))
+            elseif MetObj == "Double2"
+                return (sum((YYsl .- Y_obs).^2)/length(YYsl)/(var(YYsl)+var(Y_obs)+(mean(YYsl)-mean(Y_obs))^2), 1 -  abs(sum((YYsl.-mean(YYsl)).*(Y_obs .- mean(Y_obs)))/(std(YYsl)*std(Y_obs)*length(YYsl))))
+            elseif MetObj == "Double3"
+                return (abs(sqrt(mean((YYsl .- Y_obs).^2))/5), 1 -  abs(sum((YYsl.-mean(YYsl)).*(Y_obs .- mean(Y_obs)))/(std(YYsl)*std(Y_obs)*length(YYsl))))
+            end
+        end
+
+        
+        boundsr = [(log(1e-3), log(5e-1)),
+                    (log(1e-1), log(1e+2)),
+                    (log(1e-5), log(1e-1)),
+                    (log(1e-5), log(1e-1))]
+
+        if MetObj == "Double" || MetObj == "Double2" || MetObj == "Double3"
+            resr = bboptimize(Calibra_MDr; 
+                            # Method = :simultaneous_perturbation_stochastic_approximation,
+                            SearchRange = boundsr,
+                            NumDimensions = 4,
+                            PopulationSize = 500,
+                            MaxSteps = 5000,
+                            FitnessTolerance = 1e-6,
+                            FitnessScheme=ParetoFitnessScheme{2}(is_minimizing=true),
+                            TraceMode=:compact,
+                            ϵ=0.1,
+                            τ = 0.05,
+                            MaxStepsWithoutEpsProgress = 10000,
+                            Method=:borg_moea)
+        elseif MetObj == "Triple"
+            resr = bboptimize(Calibra_MDr; 
+                            # Method = :simultaneous_perturbation_stochastic_approximation,
+                            SearchRange = boundsr,
+                            NumDimensions = 4,
+                            PopulationSize = 500,
+                            MaxSteps = 5000,
+                            FitnessTolerance = 1e-6,
+                            FitnessScheme=ParetoFitnessScheme{3}(is_minimizing=true),
+                            TraceMode=:compact,
+                            ϵ=0.1,
+                            τ = 0.05,
+                            MaxStepsWithoutEpsProgress = 10000,
+                            Method=:borg_moea)
+        else
+            resr = bboptimize(Calibra_MDr; 
+                            Method = :adaptive_de_rand_1_bin,
+                            SearchRange = boundsr,
+                            NumDimensions = 4,
+                            PopulationSize = 500,
+                            MaxSteps = 5000,
+                            FitnessTolerance = 1e-6,
+                            TraceMode=:compact,
+                            ϵ=0.1,
+                            τ = 0.05,
+                            MaxStepsWithoutEpsProgress = 10000)
+        end
+
+        objr = best_fitness(resr)
+        popr = best_candidate(resr)
+
+        Ymdr = Yates09(E, dt, -exp(popr[1]), exp(popr[2]), -exp(popr[3]), -exp(popr[4]), Y_obs[1])
+
+        Ysl = Ymdr[idx_obs]
+        aRP = sum((Ysl.-mean(Ysl)).*(Y_obs .- mean(Y_obs)))/(std(Ysl)*std(Y_obs)*length(Ysl))
+        aRMSE = sqrt(mean((Ysl .- Y_obs).^2))
+        aMSS = 1 - sum((Ysl .- Y_obs).^2)/length(Ysl)/(var(Ysl)+var(Y_obs)+(mean(Ysl)-mean(Y_obs))^2)
+
+
+        println("\n\n****************Finished****************\n\n")
+
+        year_atts = Dict("long_name" => "Year")
+        month_atts = Dict("long_name" => "Month")
+        day_atts = Dict("long_name" => "Day")
+        hour_atts = Dict("long_name" => "Hour")
+        println("Writing output...")
+
+        output = wrkDir*"/results/Shoreline_MD.nc"
+        nccreate(output, "year",
+                    "dim", length(YY),
+                    atts = year_atts)
+        ncwrite(YY, output, "year")
+        nccreate(output, "month",
+                    "dim", length(MM),
+                    atts = month_atts)
+        ncwrite(MM, output, "month")
+        nccreate(output, "day",
+                    "dim", length(DD),
+                    atts = day_atts)
+        ncwrite(DD, output, "day")
+        nccreate(output, "hour",
+                    "dim", length(HH),
+                    atts = hour_atts)
+        ncwrite(HH, output, "hour")  
+
+        Y_atts = Dict("units" => "m",
+            "long_name" => "Shoreline position",
+            "standard_name" => "Y")
+        Yi_atts = Dict("units" => "m",
+            "long_name" => "Initial shoreline position",
+            "standard_name" => "Yi")
+        kacr_atts = Dict("units" => "-",
+            "long_name" => "Accretion coefficient",
+            "standard_name" => "cacr")
+        kero_atts = Dict("units" => "-",
+            "long_name" => "Erosion coefficient",
+            "standard_name" => "cero")
+        a_atts = Dict("units" => "-",
+            "long_name" => "a parameter",
+            "standard_name" => "a")
+        b_atts = Dict("units" => "-",
+            "long_name" => "b parameter",
+            "standard_name" => "b")
+        RP_atts = Dict("units" => "-",
+            "long_name" => "Pearson correlation coefficient",
+            "standard_name" => "RP")
+        RMSE_atts = Dict("units" => "m",
+            "long_name" => "Root mean square error",
+            "standard_name" => "RMSE")
+        MSS_atts = Dict("units" => "-",
+            "long_name" => "Mielke Skill Score",
+            "standard_name" => "MSS")
+
+
+        nccreate(output, "Y",
+                    "dim", length(Ymdr),
+                    atts = Y_atts)
+        ncwrite(Ymdr, output, "Y")
+        nccreate(output, "Yi",
+                    "len", 1,
+                    atts = Yi_atts)
+        ncwrite([Y_obs[1]], output, "Yi")
+        nccreate(output, "a",
+                    "len", 1,
+                    atts = a_atts)
+        ncwrite([exp(popr[1])], output, "a")
+        nccreate(output, "b",
+                    "len", 1,
+                    atts = b_atts)
+        ncwrite([exp(popr[2])], output, "b")
+        nccreate(output, "cacr",
+                    "len", 1,
+                    atts = kacr_atts)
+        ncwrite([exp(popr[3])], output, "cacr")
+        nccreate(output, "cero",
+                    "len", 1,
+                    atts = kero_atts)
+        ncwrite([exp(popr[4])], output, "cero")
+        nccreate(output, "RP",
+                    "len", 1,
+                    atts = RP_atts)
+        ncwrite([aRP], output, "RP")
+        nccreate(output, "RMS",
+                    "len", 1,
+                    atts = RMSE_atts)
+        ncwrite([aRMSE], output, "RMSE_flagP="*string(i))
+        nccreate(output, "MSS_flagP="*string(i),
+                    "len", 1,
+                    atts = MSS_atts)
+        ncwrite([aMSS], output, "MSS_flagP="*string(i))
+
+    elseif calPar == 5
+        function Calibra_MDr(Χ)
+
+            Ymd = Yates09(E, dt, -exp(Χ[1]), exp(Χ[2]), -exp(Χ[3]), -exp(Χ[4]), Χ[5])
+            YYsl = Ymd[idx_obs]
+            if MetObj == "Pearson"
+                return 1 -  abs(sum((YYsl.-mean(YYsl)).*(Y_obs .- mean(Y_obs)))/(std(YYsl)*std(Y_obs)*length(YYsl)))
+            elseif MetObj == "RMSE"
+                return abs(sqrt(mean((YYsl .- Y_obs).^2))/5)
+            elseif MetObj == "MSS"
+                return sum((YYsl .- Y_obs).^2)/length(YYsl)/(var(YYsl)+var(Y_obs)+(mean(YYsl)-mean(Y_obs))^2)
+            elseif MetObj == "BSS"
+                return (mean((YYsl .- Y_obs).^2) - mean((YYref .- Y_obs).^2))/mean((YYref .- Y_obs).^2)
+            elseif MetObj == "Double"
+                return (sum((YYsl .- Y_obs).^2)/length(YYsl)/(var(YYsl)+var(Y_obs)+(mean(YYsl)-mean(Y_obs))^2), abs(sqrt(mean((YYsl .- Y_obs).^2))/5))
+            elseif MetObj == "Triple"
+                return (sum((YYsl .- Y_obs).^2)/length(YYsl)/(var(YYsl)+var(Y_obs)+(mean(YYsl)-mean(Y_obs))^2), abs(sqrt(mean((YYsl .- Y_obs).^2))/5), 1 -  abs(sum((YYsl.-mean(YYsl)).*(Y_obs .- mean(Y_obs)))/(std(YYsl)*std(Y_obs)*length(YYsl))))
+            elseif MetObj == "Double2"
+                return (sum((YYsl .- Y_obs).^2)/length(YYsl)/(var(YYsl)+var(Y_obs)+(mean(YYsl)-mean(Y_obs))^2), 1 -  abs(sum((YYsl.-mean(YYsl)).*(Y_obs .- mean(Y_obs)))/(std(YYsl)*std(Y_obs)*length(YYsl))))
+            elseif MetObj == "Double3"
+                return (abs(sqrt(mean((YYsl .- Y_obs).^2))/5), 1 -  abs(sum((YYsl.-mean(YYsl)).*(Y_obs .- mean(Y_obs)))/(std(YYsl)*std(Y_obs)*length(YYsl))))
+            end
+        end
+
+        
+        boundsr = [(log(1e-3), log(5e-1)),
+                    (log(1e-1), log(1e+2)),
+                    (log(1e-5), log(1e-1)),
+                    (log(1e-5), log(1e-1)),
+                    (0.25*minimum(Y_obs), 2*maximum(Y_obs))]
+
+        if MetObj == "Double" || MetObj == "Double2" || MetObj == "Double3"
+            resr = bboptimize(Calibra_MDr; 
+                            # Method = :simultaneous_perturbation_stochastic_approximation,
+                            SearchRange = boundsr,
+                            NumDimensions = 5,
+                            PopulationSize = 500,
+                            MaxSteps = 5000,
+                            FitnessTolerance = 1e-6,
+                            FitnessScheme=ParetoFitnessScheme{2}(is_minimizing=true),
+                            TraceMode=:compact,
+                            ϵ=0.1,
+                            τ = 0.05,
+                            MaxStepsWithoutEpsProgress = 10000,
+                            Method=:borg_moea)
+        elseif MetObj == "Triple"
+            resr = bboptimize(Calibra_MDr; 
+                            # Method = :simultaneous_perturbation_stochastic_approximation,
+                            SearchRange = boundsr,
+                            NumDimensions = 5,
+                            PopulationSize = 500,
+                            MaxSteps = 5000,
+                            FitnessTolerance = 1e-6,
+                            FitnessScheme=ParetoFitnessScheme{3}(is_minimizing=true),
+                            TraceMode=:compact,
+                            ϵ=0.1,
+                            τ = 0.05,
+                            MaxStepsWithoutEpsProgress = 10000,
+                            Method=:borg_moea)
+        else
+            resr = bboptimize(Calibra_MDr; 
+                            Method = :adaptive_de_rand_1_bin,
+                            SearchRange = boundsr,
+                            NumDimensions = 5,
+                            PopulationSize = 500,
+                            MaxSteps = 5000,
+                            FitnessTolerance = 1e-6,
+                            TraceMode=:compact,
+                            ϵ=0.1,
+                            τ = 0.05,
+                            MaxStepsWithoutEpsProgress = 10000)
+        end
+
+        objr = best_fitness(resr)
+        popr = best_candidate(resr)
+
+        Ymdr = Yates09(E, dt, -exp(popr[1]), exp(popr[2]), -exp(popr[3]), -exp(popr[4]), popr[5])
+
+        Ysl = Ymdr[idx_obs]
+        aRP = sum((Ysl.-mean(Ysl)).*(Y_obs .- mean(Y_obs)))/(std(Ysl)*std(Y_obs)*length(Ysl))
+        aRMSE = sqrt(mean((Ysl .- Y_obs).^2))
+        aMSS = 1 - sum((Ysl .- Y_obs).^2)/length(Ysl)/(var(Ysl)+var(Y_obs)+(mean(Ysl)-mean(Y_obs))^2)
+
+        println("\n\n****************Writing output****************\n\n")
+
+        year_atts = Dict("long_name" => "Year")
+        month_atts = Dict("long_name" => "Month")
+        day_atts = Dict("long_name" => "Day")
+        hour_atts = Dict("long_name" => "Hour")
+        println("Writing output...")
+
+        output = wrkDir*"/results/Shoreline_MD.nc"
+        nccreate(output, "year",
+                    "dim", length(YY),
+                    atts = year_atts)
+        ncwrite(YY, output, "year")
+        nccreate(output, "month",
+                    "dim", length(MM),
+                    atts = month_atts)
+        ncwrite(MM, output, "month")
+        nccreate(output, "day",
+                    "dim", length(DD),
+                    atts = day_atts)
+        ncwrite(DD, output, "day")
+        nccreate(output, "hour",
+                    "dim", length(HH),
+                    atts = hour_atts)
+        ncwrite(HH, output, "hour")  
+
+        Y_atts = Dict("units" => "m",
+            "long_name" => "Shoreline position",
+            "standard_name" => "Y")
+        Yi_atts = Dict("units" => "m",
+            "long_name" => "Initial shoreline position",
+            "standard_name" => "Yi")
+        kacr_atts = Dict("units" => "-",
+            "long_name" => "Accretion coefficient",
+            "standard_name" => "cacr")
+        kero_atts = Dict("units" => "-",
+            "long_name" => "Erosion coefficient",
+            "standard_name" => "cero")
+        a_atts = Dict("units" => "-",
+            "long_name" => "a parameter",
+            "standard_name" => "a")
+        b_atts = Dict("units" => "-",
+            "long_name" => "b parameter",
+            "standard_name" => "b")
+        RP_atts = Dict("units" => "-",
+            "long_name" => "Pearson correlation coefficient",
+            "standard_name" => "RP")
+        RMSE_atts = Dict("units" => "m",
+            "long_name" => "Root mean square error",
+            "standard_name" => "RMSE")
+        MSS_atts = Dict("units" => "-",
+            "long_name" => "Mielke Skill Score",
+            "standard_name" => "MSS")
+
+        nccreate(output, "Y",
+                    "dim", length(Ymdr),
+                    atts = Y_atts)
+        ncwrite(Ymdr, output, "Y")
+        nccreate(output, "Yi",
+                    "len", 1,
+                    atts = Yi_atts)
+        ncwrite([popr[5]], output, "Yi")
+        nccreate(output, "a",
+                    "len", 1,
+                    atts = a_atts)
+        ncwrite([exp(popr[1])], output, "a")
+        nccreate(output, "b",
+                    "len", 1,
+                    atts = b_atts)
+        ncwrite([exp(popr[2])], output, "b")
+        nccreate(output, "cacr",
+                    "len", 1,
+                    atts = kacr_atts)
+        ncwrite([exp(popr[3])], output, "cacr")
+        nccreate(output, "cero",
+                    "len", 1,
+                    atts = kero_atts)
+        ncwrite([exp(popr[4])], output, "cero")
+        nccreate(output, "RP",
+                    "len", 1,
+                    atts = RP_atts)
+        ncwrite([aRP], output, "RP")
+        nccreate(output, "RMS",
+                    "len", 1,
+                    atts = RMSE_atts)
+        ncwrite([aRMSE], output, "RMSE_flagP="*string(i))
+        nccreate(output, "MSS_flagP="*string(i),
+                    "len", 1,
+                    atts = MSS_atts)
+        ncwrite([aMSS], output, "MSS_flagP="*string(i))
+
+    end
+
+    println("\n\n****************Finished****************\n\n")
 
 end
